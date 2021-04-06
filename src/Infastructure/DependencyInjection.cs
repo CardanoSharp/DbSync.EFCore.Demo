7dab37cec8bccb2ca2d11e8639230bf067ea6264
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-05 22:17:16 -0700

    Reference updates

--- src/Infastructure/DependencyInjection.cs
+++ src/Infastructure/DependencyInjection.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infastructure
 {
-    class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
     }
 }
