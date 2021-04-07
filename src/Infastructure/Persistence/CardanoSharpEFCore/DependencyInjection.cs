25b4246802dd3b27ad23eccf4163b21343d3fe47
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-06 19:07:17 -0700

    More persistance layer changes and interface additions

--- src/Infastructure/Persistence/CardanoSharpEFCore/DependencyInjection.cs
+++ src/Infastructure/Persistence/CardanoSharpEFCore/DependencyInjection.cs
@@ -2,11 +2,21 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using Npgsql;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=yourPassword;Database=mydb;");
+        var connectionString = "User ID=postgrest_ro;Password=CHANGEME;Host=localhost;Port=5432;Database=cdbs;";
+
+        var builder = new NpgsqlConnectionStringBuilder(connectionString);
+
+        services.AddDbContext<IApplicationCardanoSharpEFCoreDbContext>(options => options.UseNpgsql(builder.ConnectionString));
+
+        return services; 
+
+
     }
 }
