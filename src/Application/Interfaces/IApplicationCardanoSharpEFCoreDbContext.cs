aa0cfb86d5c2ab56562aaa4cc3a01ec1d46da7ae
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-08 18:50:29 -0700

    Reference Updates

--- src/Application/Interfaces/IApplicationCardanoSharpEFCoreDbContext.cs
+++ src/Application/Interfaces/IApplicationCardanoSharpEFCoreDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CardanoSharp.DbSync.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,6 @@ namespace Application.Interfaces
 {
     public interface IApplicationCardanoSharpEFCoreDbContext 
     {
-        cardanoContext
+        CardanoContext
     }
 }
