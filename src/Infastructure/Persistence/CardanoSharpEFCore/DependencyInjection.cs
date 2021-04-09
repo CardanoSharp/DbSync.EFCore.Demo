364411d19e2921f406c840b9963aa84d7d3c7810
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-08 17:08:17 -0700

    Reference updates

--- src/Infastructure/Persistence/CardanoSharpEFCore/DependencyInjection.cs
+++ src/Infastructure/Persistence/CardanoSharpEFCore/DependencyInjection.cs
@@ -4,16 +4,16 @@ using System;
 using Npgsql;
 using Application.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using CardanoDbSharp;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = "User ID=postgrest_ro;Password=CHANGEME;Host=localhost;Port=5432;Database=cdbs;";
 
-        var builder = new NpgsqlConnectionStringBuilder(connectionString);
+        var builder = new NpgsqlConnectionStringBuilder(configuration.GetConnectionString("Cardano"));
 
-        services.AddDbContext<IApplicationCardanoSharpEFCoreDbContext>(options => options.UseNpgsql(builder.ConnectionString));
+        services.AddDbContext<CardanoContext>(options => options.UseNpgsql(builder.ConnectionString));
 
         return services; 
 
