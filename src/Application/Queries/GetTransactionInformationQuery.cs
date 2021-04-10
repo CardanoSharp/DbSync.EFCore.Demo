3a740ec5276f28005a2766bbfec3d8dc5a79e1fe
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-10 00:18:53 -0700

    More Persistence layer changes

--- src/Application/Queries/GetTransactionInformationQuery.cs
+++ src/Application/Queries/GetTransactionInformationQuery.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Application.Queries
 {
-    class GetTransactionQuery
-    {
-        public record TransactionInformation();
-    }
+
+    public record GetTransactionInformationQuery(DateTime DateTime, short Epoch, int Block, int Confirmations, double TransactionID, List<string> Addresses, double TotalOutput, double TransactionFee) : IRequest<GetTransactionInformationQuery>;
+
 }
