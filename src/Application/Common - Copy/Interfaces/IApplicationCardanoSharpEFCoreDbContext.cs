0bf211d859efefd9dd85610a346895e09b3e9c08
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-09 23:38:56 -0700

    Clean Solution

--- src/Application/Common - Copy/Interfaces/IApplicationCardanoSharpEFCoreDbContext.cs
+++ src/Application/Common - Copy/Interfaces/IApplicationCardanoSharpEFCoreDbContext.cs
@@ -1,12 +1,4 @@
-﻿using CardanoSharp.DbSync.EntityFramework;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Interfaces
+﻿namespace Application.Interfaces
 {
     public interface IApplicationCardanoSharpEFCoreDbContext
     {
