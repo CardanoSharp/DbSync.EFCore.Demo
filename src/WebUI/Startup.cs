920cccc3740610a9c8e63c11b5070f6f55dda86f
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-11 15:05:57 -0700

    Redoing Files

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Application.Queries.GetWeatherForecasts;
+using Application;
 
 namespace WebUI
 {
@@ -20,6 +21,8 @@ namespace WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
+            services.AddInfrastructure(Configuration);
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
