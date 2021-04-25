0e5d27908f43556370beebff13eb8bab19e73449
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-25 13:29:58 -0700

    Adding Unit Tests

---
+++ tests/ApplicationIntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ApplicationIntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
