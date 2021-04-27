8f77d55e23d824972bff98a7341d175c82c699f2
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-26 18:30:29 -0700

    Added Db connection test

--- tests/ApplicationIntegrationTests/DatabaseFixture.cs
+++ tests/ApplicationIntegrationTests/DatabaseFixture.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using CardanoSharp.DbSync.EntityFramework;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CardanoSharp.DbSync.EntityFramework.Models;
+using Moq;
+using Npgsql;
+using System;
+using System.IO;
+using WebUI;
+using Xunit;
 using System.Threading.Tasks;
 
 namespace ApplicationIntegrationTests
 {
-    class TestBase
+    public class DatabaseFixture : IDisposable
     {
+        private readonly CardanoContext cardanoContext;
+        private static readonly IConfiguration _configuration;
+
+ 
+        public DatabaseFixture()
+        {
+            var builder = new ConfigurationBuilder()
+                        .Build();
+
+            var options = new DbContextOptionsBuilder<CardanoContext>()
+                      .UseInMemoryDatabase(databaseName: "Cardano")
+                      .Options;
+
+            cardanoContext = new CardanoContext(options, builder);
+        }
+
+        [Fact]
+        public async Task FirstTimeSetUp()
+        {
+            //arrange
+            var block = new Block()
+            {
+                EpochNo = 1
+            };
+
+            cardanoContext.Blocks.Add(block);
+            await cardanoContext.SaveChangesAsync();
+
+            //act
+            var firstBlock = await cardanoContext.Blocks.FirstOrDefaultAsync();
+
+            //assert
+            Assert.True(firstBlock.EpochNo is 1); 
+        }
+
+        public async void Dispose()
+        {
+            await cardanoContext.DisposeAsync();
+        }
     }
 }
