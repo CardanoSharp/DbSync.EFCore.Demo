684a2ada579cfe9d0e7b2f9420a52b98dba155f8
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-04-27 19:20:30 -0700

    Added Epoch Test

--- tests/ApplicationIntegrationTests/BlockChainTransactionsTest/UserEnteredTransactionsPerEpochTest.cs
+++ tests/ApplicationIntegrationTests/BlockChainTransactionsTest/UserEnteredTransactionsPerEpochTest.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ApplicationIntegrationTests.Builders;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace ApplicationIntegrationTests.BlockChainTransactionsTest
 {
-    class UserEnteredTransactionsPerEpochTest
+    public class UserEnteredTransactionsPerEpochTest
     {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(10)]
+        public void GetUserEpochTest(int userEpoch)
+        {
+             BlockBuilder.GenerateBlocks(15);
 
+            var epoch =   BlockBuilder.BlockList.Where(s => s.EpochNo == userEpoch).FirstOrDefault();
+
+            Assert.Equal(userEpoch, epoch.EpochNo);
+        }
     }
 }
---
+++ tests/ApplicationIntegrationTests/Builders/BlockBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using CardanoSharp.DbSync.EntityFramework.Models;
+
+namespace ApplicationIntegrationTests.Builders
+{
+    public static class BlockBuilder
+    {
+        public static List<Block> BlockList = new List<Block>();
+
+        public static void GenerateBlocks(int numberOfBlocks)
+        {
+            var random = new Randomizer();
+
+            for (int i = 0; i < numberOfBlocks; i++)
+            {
+                var block =  new Block
+                {
+                    EpochNo = i,
+                    TxCount = random.Number(1, 1000),
+                };
+
+                 BlockList.Add(block); 
+
+            }
+        }
+    }
+}
